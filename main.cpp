e62d0d180065078917d7b2c9b5b6adcb18b52898
이승민 <seungmin2866@gmail.com>
2026-09-07 10:58:20 +0900

    202503325 이승민

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 
 using namespace std;
 
-// 학번:
-// 이름:
+// 학번: 202503325
+// 이름: 이승민
 
 int main(int argc, const char *argv[]) {
     // TODO: 아래 두 줄을 정확히 출력하도록 코드를 작성하세요.
     //
     //   Hello, OOP!
     //   C++ is ready.
+    cout << "Hello, OOP!" << endl;
+    cout << "C++ is ready." << endl;
 
     return 0;
 }
